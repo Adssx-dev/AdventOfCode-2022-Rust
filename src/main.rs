45e70b57c5e0de59f7053f978c4cb83671a928d7
Adssx <adssx-dev@gmail.com>
2022-12-01 18:50:41 +0100

    Solving day 1

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use problems::day1::{self, day1_pt1};
+
+mod problems;
+use crate::day1::*;
+
+
+
+fn main() {
+    day1_pt2();
+}
---
+++ src/problems/day1.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+pub fn load_data() -> Vec<i32> {
+    let mut vec = vec!(0);
+
+    let file = File::open("inputs/day1.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(str) => match str.as_str() { 
+                "" => vec.push(0),
+                number_as_str => {
+                    *(vec.last_mut().unwrap()) = vec.last().unwrap() + number_as_str.parse::<i32>().unwrap();
+                }
+            }
+            Err(E) => panic!("Could not load file"),
+        }
+    }
+    vec
+}
+
+pub fn day1_pt1() {
+    let vec = load_data();
+
+    println!("{}", vec.iter().max().unwrap());
+}
+
+pub fn day1_pt2() {
+    let mut vec = load_data();
+
+    vec.sort_by(|a, b| b.cmp(a));
+
+    let top3 : i32 = vec[0..3].iter().sum();
+        
+    println!("{}", top3);
+}
+
