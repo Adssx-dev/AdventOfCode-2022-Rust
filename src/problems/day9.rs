0d4ba3cdc3ded1ac0b53804aca02ad384460aed0
Adssx <adssx.dev@gmail.com>
2022-12-09 19:20:11 +0100

    Solving day 9 part 2

--- src/problems/day9.rs
+++ src/problems/day9.rs
@@ -55,27 +55,30 @@ impl Movement {
 
 #[derive(Debug)]
 struct Board {
-    head_pos : Position,
-    tail_pos : Position,
-    rope_length : i32,
+    knots_pos : Vec<Position>,
     all_tail_positions : Vec<Position>
 }
 
 impl Board {
-    pub fn new(rope_length : i32) -> Board {
+    pub fn new(rope_length : usize) -> Board {
         Board {
-            head_pos : Position { x: 0, y: 0 },
-            tail_pos : Position { x: 0, y: 0 },
-            rope_length,
+            knots_pos : vec![Position { x: 0, y: 0 }; rope_length],
             all_tail_positions : vec!()
         }
     }
 
     pub fn move_head (&mut self, mov : &Movement) {
-        self.head_pos.move_pos(mov);
-        if self.tail_pos.distance(&self.head_pos) > self.rope_length {
-            self.tail_pos.move_towards(&self.head_pos);
-            self.record_position(&self.tail_pos.clone());
+        self.knots_pos[0].move_pos(mov);
+
+        // DO not check head here !
+        for k in 1..self.knots_pos.len() {
+            let previous_pos = &self.knots_pos[k-1].clone();
+            if self.knots_pos[k].distance(previous_pos) > 1 {
+                self.knots_pos[k].move_towards(previous_pos);
+                if k == self.knots_pos.len() - 1 {
+                    self.record_position(&self.knots_pos[k].clone());
+                }
+            }
         }
 
     }
@@ -116,5 +119,33 @@ pub fn day9_pt1 () -> usize {
 pub fn day9_pt2 () -> usize {
     let file = include_str!("../../inputs/day9.txt");
 
-    0
+    let movements = file.lines()
+        .map(|l| Movement::new(l))
+        .flatten();
+
+    let mut board = Board::new(10);
+
+    for m in movements {
+        board.move_head(&m);
+    }
+    board.unique_positions_count() + 1
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::problems::day9::*;
+
+    #[test]
+    fn day9_pt1_test() {
+        let result = day9_pt1();
+        assert_eq!(result, 6332);
+    }
+
+    #[test]
+    fn day9_pt2_test() {
+        let result = day9_pt2();
+        assert_eq!(result, 2511);
+    }
 }
